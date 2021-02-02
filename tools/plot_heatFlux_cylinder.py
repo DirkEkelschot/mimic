e19331b2a7122405b983b18153376cd2ff0d823c
Dirk <d.ekelschot@gmail.com>
2021-02-01 17:02:45 -0800

    added the plot convergence tool to the repo.

--- tools/plot_heatFlux_cylinder.py
+++ tools/plot_heatFlux_cylinder.py
@@ -165,71 +165,27 @@ def PlotSpanWall(fname,type,hoffset,nEl,Npo,n_z,fign,c,l,lab,pl):
 
 
 
-fname1 = 'qw_Cgrid_h_0p005.dat'
-fname2 = 'qw_Cgrid_h_0p010.dat'
-fname3 = 'qw_Cgrid_h_0p0125.dat'
-fname33 = 'qw_turb1.dat'
-fname4 = 'qw_Cgrid_h_0p0150.dat'
-fname5 = 'qw_Cgrid_h_0p020.dat'
-fname6 = 'qw_L_f1p0m5.dat'
-fname7 = 'qw_tess.dat'
-fname8 = 'qw_tess_plus.dat'
 
-fname9 = 'qw_Cgrid_f_tess_plus.dat'
-fname10 = 'qw_Cgrid_f.dat'
-
-fname11 = 'qw_fail.dat'
-fname12 = 'qqw_hz_0p05.dat'
 
 fnamer0 = 'qw_base.dat'
 fnamer1 = 'qw_Cgrid_f.dat'
 fnamer = 'qw_reference.dat'
-
-pl1 = []
-PlotSpanWall(fname1,3,11,1188,700,7,1,[0.5,0.0,0.1],2,'$f$ = 0.005 -> ',pl1);
-PlotSpanWall(fnamer,4,11,2394,2800,7,1,[0.01,0.01,0.01],2,'ref hex -> ',pl1);
-plt.grid()
-pl2 = []
-PlotSpanWall(fname2,3,11,1188,700,7,2,[0.5,0.0,0.1],2,'$f$ = 0.01 -> ',pl2);
-PlotSpanWall(fnamer,4,11,2394,2800,7,2,[0.01,0.01,0.01],2,'ref hex -> ',pl2);
-plt.grid()
-pl3 = []
-PlotSpanWall(fname3,3,11,1188,700,7,3,[0.5,0.0,0.1],2,'$f$ = 0.0125 -> ',pl3);
-PlotSpanWall(fnamer,4,11,2394,2800,7,3,[0.01,0.01,0.01],2,'ref hex -> ',pl3);
-plt.grid();
-pl4 = []
-PlotSpanWall(fname33,3,11,1188,700,7,4,[0.5,0.0,0.1],2,'$f$ = 0.015 ->',pl4);
-PlotSpanWall(fnamer,4,11,2394,2800,7,4,[0.01,0.01,0.01],2,'ref hex -> ',pl4);
-pl5 = []
-PlotSpanWall(fname5,3,11,1188,700,7,5,[0.5,0.0,0.1],2,'$f$ = 0.02 ->',pl5);
-PlotSpanWall(fnamer,4,11,2394,2800,7,5,[0.01,0.01,0.01],2,'ref hex -> ',pl5);
-pl6 = []
-PlotSpanWall(fname6,3,11,1188,700,7,6,[0.5,0.0,0.1],2,'length var ->',pl6);
-PlotSpanWall(fnamer,4,11,2394,2800,7,6,[0.01,0.01,0.01],2,'ref hex -> ',pl6);
-pl7 = []
-PlotSpanWall(fname7,3,11,1188,700,7,7,[0.5,0.0,0.1],2,'tess ->',pl7);
-PlotSpanWall(fnamer,4,11,2394,2800,7,7,[0.01,0.01,0.01],2,'ref hex -> ',pl7);
-pl8 = []
-PlotSpanWall(fname8,3,11,1188,700,7,8,[0.5,0.0,0.1],2,'tess +',pl8);
-PlotSpanWall(fnamer,4,11,2394,2800,7,8,[0.01,0.01,0.01],2,'ref hex -> ',pl8);
-
-pl9 = []
-PlotSpanWall(fname9,3,11,2388,1400,7,9,[0.5,0.0,0.1],2,'tess +',pl9);
-PlotSpanWall(fname10,4,11,1194,1400,7,9,[0.01,0.01,0.01],2,'ref hex -> ',pl9);
-
+fname_a = 'qw_gmam.dat'
+fname_a2 = 'qw_gmam_fo.dat'
 
 pl10 = []
 PlotSpanWall(fnamer0,4,11,594,700,7,11,[0.0,0.0,0.11],2,'ref 0',pl10);
 PlotSpanWall(fnamer1,4,11,1194,1400,7,11,[0.00,0.11,0.00],2,'ref 1',pl10);
 PlotSpanWall(fnamer, 4,11,2394,2800,7,11,[0.11,0.00,0.00],2,'ref 2',pl10);
 
-pl11 = []
-PlotSpanWall(fname11,3,11,2388,1400,7,12,[0.5,0.0,0.1],2,'tess +',pl11);
-PlotSpanWall(fnamer,4,11,2394,2800,7,12,[0.01,0.01,0.01],2,'ref hex -> ',pl11);
 
 pl12 = []
-PlotSpanWall(fname12,3,11,1188,700,7,13,[0.5,0.0,0.1],2,'tess +',pl12);
+PlotSpanWall(fname_a,3,11,1188,700,7,13,[0.5,0.0,0.1],2,'tess +',pl12);
 PlotSpanWall(fnamer,4,11,2394,2800,7,13,[0.01,0.01,0.01],2,'ref hex -> ',pl12);
+
+pl13 = []
+PlotSpanWall(fname_a2,3,11,1188,700,7,14,[0.5,0.0,0.1],2,'tess +',pl13);
+PlotSpanWall(fnamer,4,11,2394,2800,7,14,[0.01,0.01,0.01],2,'ref hex -> ',pl13);
 
 plt.grid()
 plt.show()
